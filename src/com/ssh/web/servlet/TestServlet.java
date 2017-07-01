033371a75522aba52fe2922d1ed0de6ebbbc5f05
dh <dh@163.com>
2017-07-01 19:39:28 +0800

    添加字符编码集

--- src/com/ssh/web/servlet/TestServlet.java
+++ src/com/ssh/web/servlet/TestServlet.java
@@ -14,6 +14,7 @@ public class TestServlet extends HttpServlet {
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		response.setCharacterEncoding("utf-8");
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
